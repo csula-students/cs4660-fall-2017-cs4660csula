84ffef0d8369cd1e661eaf10dbf6ab9c38bb9adf
cs4660csula <cs4660.usa@gmail.com>
2017-09-08 21:34:28 -0700

    finishing up exercises

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -1,4 +1,4 @@
-"""Files tests simple file read related operations"""
+# git """Files tests simple file read related operations"""
 
 class SimpleFile(object):
     """SimpleFile tests using file read api to do some simple math"""
@@ -7,13 +7,18 @@ class SimpleFile(object):
         """
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
+
         """
+        with open(file_path) as f:
+            for line in f:
+                self.numbers.append(float(i) for i in line.split())
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
+        
         pass
 
     def get_max(self, line_number):
@@ -21,6 +26,7 @@ class SimpleFile(object):
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
+        return max(self.numbers[line_number])
         pass
 
     def get_min(self, line_number):
